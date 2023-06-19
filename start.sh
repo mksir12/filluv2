cf9c2560452afdc9a79f9fddea4f57bbd08fc75c
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-19 23:28:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Private-Botz/Elsa-Bot.git /Elsa-Bot
+  git clone https://github.com/mksir12/filluv2.git /Elsa-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Elsa-Bot
