b894e26c7e19b10d7ae6e359ac36dee46e3f6d09
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-21 15:36:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,14 +4,14 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 Here is the help for my COMMANDS.""" 
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/tgitzmedawn'>TG ITZ ME DAWN</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: Railway
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+👦🏼 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/tgitzmedawn'>TG ITZ ME DAWN</a>
+📚 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+🗣️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+📃 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+📡 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: Railway
+🗒️ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- This Bot is an open source project. 
+- This Bot is special source project. 
 - Source - <a href='https://t.me/tgitzmedawn'>TG ITZ ME DAWN</a>  
 
 <b>DEVS:</b>
