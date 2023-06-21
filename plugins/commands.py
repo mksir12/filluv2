ced08f487c27110ea69d9256746bcf115e57b881
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-21 15:29:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,10 +23,10 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/cinema4587')
+                InlineKeyboardButton('Updates 🤖', url='https://t.me/cinema4587')
             ],
             [
-                InlineKeyboardButton('🙆🏻 Help 🦾', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('Help 📚', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('± Add Me To Your Groups ±', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('⛦ Add Me To Your Groups ⛦', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('Search 🧐', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Updates 🤖', url='https://t.me/cinema4587')
@@ -88,7 +88,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('⛦ Add Me To Your Groups ⛦', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('Search 🧐', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Updates 🤖', url='https://t.me/cinema4587')
